3d611e6d24db55fbe24e252afe47e0e5994d9125
Vyacheslav Yashin <61734466+vyashin-devops@users.noreply.github.com>
2022-02-17 12:30:47 +0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region     = "eu-central-1"
+}
+
+resource "aws_instance" "ter_aws_linux" {
+  ami           = "ami-00f22f6155d6d92c5"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "aws_instance_by_terraform"
+  }
+}
